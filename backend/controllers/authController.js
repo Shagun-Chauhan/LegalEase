f3e02a5691193817d90f200a5b0991c967301aba
Shagun-Chauhan <165358009+Shagun-Chauhan@users.noreply.github.com>
2026-04-09 16:37:04 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -96,8 +96,8 @@ exports.verifyOtp = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
